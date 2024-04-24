bd4e3d71ba595e7cee2650cb8045c1d4b2f49cb0
ClauPailaha <s22110150@student.unklab.ac.id>
2024-04-24 23:49:21 +0800

    Adding ProfilePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,4 +3,5 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Home from './Home';
 import Schedule from './Schedule';
-export {SplashScreen, SignIn, SignUp, Schedule, Home};
+import Profile from './Profile';
+export {SplashScreen, SignIn, SignUp, Schedule, Home, Profile};
