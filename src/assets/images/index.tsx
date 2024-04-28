798456cc9517756dc7151add72d568476769b772
GivenPantouw07 <givenazarya@gmail.com>
2024-04-28 21:48:39 +0800

    feat: slicing Education page, 20% done

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,6 +1,21 @@
 import Logo2 from './Logo2.png';
-import facebook from './facebook-icon.png'
-import google from './google-icon.png'
-import Cards from './Cards .png'
+import facebook from './facebook-icon.png';
+import google from './google-icon.png';
+import Cards from './Cards .png';
+import Metals from './Metals.png';
+import Leafes from './Leafes.png';
+import Plastic from './Plastic.png';
+import FoodWaste from './Food_Waste.png';
+import BrokenGlass from './Broken_Glass.png';
 
-export {Logo2, Cards, facebook, google};
+export {
+  Logo2,
+  Cards,
+  facebook,
+  google,
+  Metals,
+  Leafes,
+  Plastic,
+  FoodWaste,
+  BrokenGlass,
+};
--- src/pages/Education/index.tsx
+++ src/pages/Education/index.tsx
@@ -1,6 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {Gap, PageHeader} from '../../components';
+import {
+  Leafes,
+  FoodWaste,
+  Plastic,
+  BrokenGlass,
+  Metals,
+} from '../../assets/images';
 
 const Education = ({navigation}) => {
   return (
@@ -19,7 +26,47 @@ const Education = ({navigation}) => {
       </View>
       <Gap height={7} />
       <View style={styles.bgcontainer2}>
-        <View style={styles.bgcontainer3} />
+        <Gap height={17} />
+        <View style={styles.bgcontainer3}>
+          <Image source={Leafes} style={styles.image1} />
+          <TouchableOpacity>
+            <Text style={styles.heading3}> NATURE WASTE </Text>
+          </TouchableOpacity>
+        </View>
+        <Gap height={5} />
+        <View style={styles.bgcontainer3}>
+          <Image source={FoodWaste} style={styles.image2} />
+          <TouchableOpacity>
+            <Text style={styles.heading3}> FOOD WASTE </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Gap height={50} />
+      <View style={styles.bg2container1}>
+        <Text style={styles.heading2}>NON ORGANIC</Text>
+      </View>
+      <Gap height={7} />
+      <View style={styles.bg2container2}>
+        <Gap height={17} />
+        <View style={styles.bgcontainer3}>
+          <Image source={Plastic} style={styles.image1} />
+          <TouchableOpacity>
+            <Text style={styles.heading3_2}> PLASTIC </Text>
+          </TouchableOpacity>
+        </View>
+        <Gap height={5} />
+        <View style={styles.bgcontainer3}>
+          <Image source={BrokenGlass} style={styles.image2} />
+          <TouchableOpacity>
+            <Text style={styles.heading3_2}> GLASS </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bgcontainer3}>
+          <Image source={Metals} style={styles.image3} />
+          <TouchableOpacity>
+            <Text style={styles.heading3_2}> METAL </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -40,14 +87,35 @@ const styles = StyleSheet.create({
     fontSize: 19,
     fontFamily: 'Poppins-Regular',
     color: '#000000',
-    paddingLeft: 7,
+    paddingLeft: 15,
+  },
+  heading3: {
+    fontSize: 20,
+    fontFamily: 'Poppins-Italic',
+    color: '#000000',
+    paddingLeft: 35,
+  },
+  heading3_2: {
+    fontSize: 20,
+    fontFamily: 'Poppins-Italic',
+    color: '#000000',
+    paddingLeft: 60,
   },
   bgcontainer1: {
     flexDirection: 'column',
     borderRadius: 8,
     borderTopRightRadius: 30,
     borderBottomLeftRadius: 30,
-    width: 110,
+    width: 120,
+    backgroundColor: '#C3FFAE',
+    marginLeft: 30,
+  },
+  bg2container1: {
+    flexDirection: 'column',
+    borderRadius: 8,
+    borderTopRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    width: 161,
     backgroundColor: '#C3FFAE',
     marginLeft: 30,
   },
@@ -55,16 +123,45 @@ const styles = StyleSheet.create({
     width: 350,
     height: 170,
     borderRadius: 10,
+    borderBottomWidth: 6,
+    borderBottomColor: 'rgba(0, 0, 0, 0.15)',
+    backgroundColor: '#00FFCB',
+    marginLeft: 30,
+    marginRight: 29,
+  },
+  bg2container2: {
+    width: 350,
+    height: 230,
+    borderRadius: 10,
+    borderBottomWidth: 6,
+    borderBottomColor: 'rgba(0, 0, 0, 0.15)',
     backgroundColor: '#00FFCB',
     marginLeft: 30,
     marginRight: 29,
   },
   bgcontainer3: {
-    marginHorizontal: 50,
-    marginTop: 10,
-    width: 250,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 28,
+    marginTop: 5,
+    width: 300,
     height: 50,
+    borderRadius: 18,
+    paddingLeft: 10,
     backgroundColor: 'white',
+  },
+  image1: {
+    width: 40,
+    height: 45,
+    marginLeft: 4,
+  },
+  image2: {
+    width: 50,
+    height: 55,
+  },
+  image3: {
+    width: 50,
+    height: 70,
   },
 });
 
