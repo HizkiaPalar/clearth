c6861d9181fb15bb4bd0596fe9bd073ea6bbcb66
Rianakeni <s22210372@student.unklab.ac.id>
2024-05-08 21:55:06 +0800

    done merging education

--- src/pages/E.Metal/index.tsx
+++ src/pages/E.Metal/index.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {PageHeader} from '../../components';
 
-const Metal = () => {
+const Metal = ({navigation}) => {
   return (
     <View>
+      <PageHeader
+        type="withPhoto5"
+        label5="Metal"
+        backButton={true}
+        onPress={() => navigation.goBack()}
+      />
       <Text>Metal</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Metal
+export default Metal;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
